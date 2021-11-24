89f49350902237f8027e3e9809733139ca243c04
vincenzopecorella <vincenzo.pecorella@mail.polimi.it>
2021-11-24 12:40:50 +0100

    Better estimation of the CO2 emissions by considering the country where the algorithms are run

---
+++ base.py
@@ -0,0 +1,79 @@
+'''
+This is the base class of cumulator.
+'''
+import json
+import time as t
+import geocoder
+import requests
+
+carbon_url = 'https://api.electricitymap.org/v3/carbon-intensity/latest?'
+private_token = 'TOKEN'
+auth = {'auth-token': private_token}
+
+
+class Cumulator:
+
+    def __init__(self):
+        self.t0 = 0
+        self.t1 = 0
+        # times are in seconds
+        self.time_list = []
+        self.cumulated_time = 0
+        # file sizes are in bytes
+        self.file_size_list = []
+        self.cumulated_data_traffic = 0
+        # number of GPU
+        self.n_gpu = 1
+        # assumptions to approximate the carbon footprint
+        # computation costs: consumption of a typical GPU in Watts converted to kWh/s
+        self.hardware_load = 250 / 3.6e6
+        # communication costs: average energy impact of traffic in a typical data centers, kWh/kB
+        self.one_byte_model = 6.894E-8
+        # conversion to carbon footprint: average carbon intensity value in gCO2eq/kWh in the EU in 2014
+        self.carbon_intensity = 447
+
+    # starts accumulating time
+    def on(self):
+        self.t0 = t.time()
+
+    # stops accumulating time and records the value
+    def off(self):
+        self.t1 = t.time()
+        self.cumulated_time += self.t1 - self.t0
+        self.time_list.append(self.t1 - self.t0)
+
+    def position_carbon_intensity(self):
+        g = geocoder.ip('me')
+        latlng = g.latlng
+        # Useless coordinates, needs testing
+        final_url = carbon_url + 'zone=zoneKey'
+        r = requests.get(final_url, headers=auth)
+        print(r.text)
+        carbon_data_requested = json.loads(r.text)
+        self.carbon_intensity = carbon_data_requested["carbonIntensity"]
+
+    # records the amount of data transferred, file_size in kilo bytes
+    def data_transferred(self, file_size):
+        self.file_size_list.append(file_size)
+        self.cumulated_data_traffic += file_size
+
+    # computes time based carbon footprint due to computations
+    def computation_costs(self):
+        return self.cumulated_time * self.n_gpu * self.hardware_load * self.carbon_intensity
+
+    # computes the carbon footprint due to communication
+    def communication_costs(self):
+        return self.one_byte_model * self.cumulated_data_traffic * self.carbon_intensity
+
+    # computes the total carbon footprint
+    def total_carbon_footprint(self):
+        return self.computation_costs() + self.communication_costs()
+
+    # prints the carbon footprint in the terminal
+    def display_carbon_footprint(self):
+        print('########\nOverall carbon footprint: %s gCO2eq\n########' %
+              "{:.2e}".format(self.total_carbon_footprint()))
+        print('Carbon footprint due to computations: %s gCO2eq' %
+              "{:.2e}".format(self.computation_costs()))
+        print('Carbon footprint due to communications: %s gCO2eq' %
+              "{:.2e}".format(self.communication_costs()))
---
+++ test.py
@@ -0,0 +1,7 @@
+from base import Cumulator
+
+a = Cumulator()
+#a.position_carbon_intensity()
+a.on()
+a.off()
+a.display_carbon_footprint()
