800a8e07bc9ce83c5ff0c050bbad7b489989dfda
giommok <giovanni.monea@epfl.ch>
2021-11-28 23:54:45 +0100

    Test

--- tests/tests.py
+++ tests/tests.py
@@ -5,6 +5,9 @@ class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, True)  # add assertion here
 
+    def test_2(self):
+        self.assertEqual(1, 1)  # add assertion here
+
 
 if __name__ == '__main__':
     unittest.main()
