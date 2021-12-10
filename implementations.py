cc8c15fbb41cfa78fe31f7391603b26471069586
giommok <giovanni.monea@epfl.ch>
2021-12-10 14:54:12 +0100

    Implemented LeNet5 on MNIST dataset

--- implementations.py
+++ implementations.py
@@ -1,7 +1,9 @@
 from sklearn.neural_network import MLPClassifier, MLPRegressor
 import pandas as pd
 from base import Cumulator
-
+import torch
+import torch.nn.functional as F
+from torch import nn
 
 def add_dataset_row(dataset, accuracy, consumption, used_algorithm, type_of_dataset='None'):
     points, features = dataset.shape
@@ -32,3 +34,27 @@ def standard_neural_network(x_training_set, y_training_set, x_test_set, y_test_s
 
     print("accuracy is: " + str(accuracy))
     cumulator.display_carbon_footprint()
+
+
+class LeNetModel(nn.Module):
+    def __init__(self, activation_function: torch.nn.functional = torch.nn.functional.relu):
+        """From: LeCun et al., 1998. Gradient-Based Learning Applied to Document Recognition"""
+        super().__init__()
+        self.activation_function = activation_function
+        self.conv1 = torch.nn.Conv2d(1, 6, kernel_size=5)
+        self.conv2 = torch.nn.Conv2d(6, 16, kernel_size=5)
+        self.conv2_drop = torch.nn.Dropout2d()
+        self.fc1 = torch.nn.Linear(256, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        max_pool2d = torch.nn.functional.max_pool2d
+
+        x = self.activation_function(max_pool2d(self.conv1(x), 2))
+        x = self.activation_function(max_pool2d(self.conv2(x), 2))
+        x = torch.flatten(x, 1)
+        x = self.activation_function(self.fc1(x))
+        x = self.activation_function(self.fc2(x))
+        x = self.fc3(x)
+        return x
