fa6a74fe23e0593017894e9e0a15f89a2aa9edbc
Stefan <stefanigescu@gmail.com>
2021-12-23 10:15:18 +0100

    clean

--- test_re.py
+++
@@ -1,9 +0,0 @@
-import re
-regexp_cpu='(Core|Ryzen).* (i\d-\d{3,5}.?|\d \d{3,5}.?)'
-cpu='Sempron 3500+'
-result=re.search(regexp_cpu, cpu)
-if result is not None:
-    print(result.group(1)+' '+result.group(2))
-else:
-    print('ops')
-    
