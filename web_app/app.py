4ce8702d420f79b144d81e05b08ac5a951426540
Giovanni Monea <giovanni.monea@epfl.ch>
2022-02-02 13:46:26 +0100

    Added first implementation for web_app

---
+++ web_app/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+FILE_PATH = 'index.html'
+
+
+# default page of our web-app
+@app.route('/')
+def home():
+    return render_template(FILE_PATH)
+
+
+# To use the predict button in our web-app
+@app.route('/predict', methods=['POST'])
+def predict():
+    output = 5
+    return render_template(FILE_PATH, prediction_image='CO2    Emission of the vehicle is :{}'.format(output))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
